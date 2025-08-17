ce21f34f777b575aa174ff6c4dc21b1098081bc7
Luke Arnone <lukeoarnone@gmail.com>
2025-08-17 17:39:38 +0100

    removed comments from section 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,6 @@
-// Step 1: Import React
 import * as React from 'react'
 import Layout from '../components/layout'
 
-// Step 2: Define your component
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
@@ -11,8 +9,6 @@ const IndexPage = () => {
   )
 }
 
-// You'll learn about this in the next task, just copy it for now
 export const Head = () => <title>Home Page</title>
 
-// Step 3: Export your component
 export default IndexPage
